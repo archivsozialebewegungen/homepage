3d36ed6fcea83c99071c8dcd85766993ca2afb0e
Michael Koltan <admin@archivsozialebewegungen.de>
2023-10-04 14:30:33 +0200

    Main methode upgradated

--- src/asb_homepage/exporter.py
+++ src/asb_homepage/exporter.py
@@ -582,10 +582,10 @@ if __name__ == '__main__':
     
     exporter = injector.get(Exporter)
     print("Starting build...")
-    #exporter.run()
-    exporter.tiny_run()
+    exporter.run()
+    #exporter.tiny_run()
     print("Starting upload...")
-    #exporter.upload()
+    exporter.upload()
     print("Finished.")
     
     #z_filter = ZeitschriftenFilter()
